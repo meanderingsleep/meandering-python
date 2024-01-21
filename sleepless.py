d4b55cfe4a3cfbb1a98feaf42498e595e984c5ad
Logan Valeski <loganvaleski@gmail.com>
2024-01-21 18:23:18 -0500

    Added Freeplay

--- sleepless.py
+++ sleepless.py
@@ -3,6 +3,8 @@ import os
 from openai import OpenAI
 import io
 from pathlib import Path
+from freeplay import Freeplay
+from freeplay.provider_config import ProviderConfig, OpenAIConfig
 
 load_dotenv() 
 
@@ -10,29 +12,29 @@ client = OpenAI(
     api_key=os.environ.get("OPENAI_API_KEY"),
 )
 
-prompt_text = open("prompt.txt").read()
+freeplay_chat = Freeplay(
+    provider_config=ProviderConfig(openai=OpenAIConfig(os.environ['OPENAI_API_KEY'])),
+    freeplay_api_key=os.environ['FREEPLAY_API_KEY'],
+    api_base=f'https://{os.environ["FREEPLAY_CUSTOMER_NAME"]}.freeplay.ai/api')
 
-# get the content that we want to convert to audio
-chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": prompt_text,
-        }
-    ],
-    model="gpt-4-1106-preview",
-)
+freeplay_environment = os.environ.get("FREEPLAY_ENVIRONMENT")
+
+chat_completion = freeplay_chat.get_completion(
+    project_id=os.environ['FREEPLAY_PROJECT_ID'],
+    template_name="story",
+    variables={},
+    tag=freeplay_environment)
 
 # create a text file so we can check the story it wrote
 bedtime_story = "bedtime_story.txt"
 with open(bedtime_story, 'w') as file:
-    file.write(chat_completion.choices[0].message.content)
+    file.write(chat_completion.content)
 
 # convert the text to audio
 response = client.audio.speech.create(
   model="tts-1-hd", # Added "hd" because its apparently better
   voice="onyx", # Onyx is the sleepiest voice in my opinion
-  input=chat_completion.choices[0].message.content
+  input=chat_completion.content
 )
 
 response.write_to_file(Path(__file__).parent / "sleepless.mp3")
