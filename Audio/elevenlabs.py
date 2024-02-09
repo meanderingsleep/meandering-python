bee4e0e7c11b4778ccff7c199b9176d6ebe835b5
Logan Valeski <loganvaleski@gmail.com>
2024-02-08 19:09:43 -0500

    Consolidated code and got merge of audio files working.

--- Audio/elevenlabs.py
+++ Audio/elevenlabs.py
@@ -8,17 +8,10 @@ from freeplay.provider_config import ProviderConfig, OpenAIConfig
 import requests
 import boto3 # For Amazon S3 uploading
 from botocore.exceptions import NoCredentialsError
+from pydub import AudioSegment
+
 
 load_dotenv() 
-
-CHUNK_SIZE = 1024
-url = "https://api.elevenlabs.io/v1/text-to-speech/zcAOhNBS3c14rBihAFp1" # Elevenlabs voice url
-
-headers = {
-  "Accept": "audio/mpeg",
-  "Content-Type": "application/json",
-  "xi-api-key": os.environ.get("XI_API_KEY")
-}
 
 client = OpenAI(
     api_key=os.environ.get("OPENAI_API_KEY"),
@@ -32,22 +25,42 @@ freeplay_chat = Freeplay(
 
 freeplay_environment = os.environ.get("FREEPLAY_ENVIRONMENT")
 
+# AWS Upload Setup
+def upload_to_aws(local_file, bucket, s3_file):
+    s3 = boto3.client('s3', aws_access_key_id=os.environ.get("ACCESS_KEY"),
+                    aws_secret_access_key=os.environ.get("SECRET_KEY"))
+
+    try:
+        s3.upload_file(local_file, bucket, s3_file)
+        print("Upload Successful")
+        return True
+    except FileNotFoundError:
+        print("The file was not found")
+        return False
+    except NoCredentialsError:
+        print("Credentials not available")
+        return False
+        
+# The initial story chunk
 story = freeplay_chat.get_completion(
     project_id=os.environ['FREEPLAY_PROJECT_ID'],
     template_name="initialize_story",
     variables={},
     tag=freeplay_environment)
 
+# Get some context from the the previous story output
 def getLast20Words(context):
-    context_words = context.split()  # Split the string into a list of words
-    last_20_words = context_words[-20:]  # Get the last 20 words
-    context = ' '.join(last_20_words)  # Join the words back into a string
+    context_words = context.split()
+    last_20_words = context_words[-20:]
+    context = ' '.join(last_20_words)
     return context
 
 context = getLast20Words(story.content)
+merged = AudioSegment.empty()
 
+# Generate 7 total chunks of text/audio
 i = 1
-while i < 7: # 7 chunks of text and audio at 3500 tokens of text each
+while i <= 50: 
     if (i != 1):
         story = freeplay_chat.get_completion(
             project_id=os.environ['FREEPLAY_PROJECT_ID'],
@@ -56,30 +69,20 @@ while i < 7: # 7 chunks of text and audio at 3500 tokens of text each
             tag=freeplay_environment)
         context = getLast20Words(story.content)
     
-    # OpenAI instead of ElevenLabs text to speech (I ran out of credits for Eleven Labs)
+    # Generate OpenAI instead of ElevenLabs text to speech 
     response = client.audio.speech.create(
-        model="tts-1-hd", # Added "hd" because its apparently better
-        voice="onyx", # Onyx is the sleepiest voice in my opinion
+        model="tts-1-hd",
+        voice="onyx",
         input=story.content
     )
 
-    response.write_to_file(Path(__file__).parent / f"output{i}.mp3")
+    # Write out the current chunk as an wav
+    response.write_to_file(Path(__file__).parent / f"output{i}.wav")
 
-    # AWS Upload Setup
-    def upload_to_aws(local_file, bucket, s3_file):
-        s3 = boto3.client('s3', aws_access_key_id=os.environ.get("ACCESS_KEY"),
-                        aws_secret_access_key=os.environ.get("SECRET_KEY"))
+    output = AudioSegment.from_file(f'output{i}.wav')
+    merged =  merged + output
 
-        try:
-            s3.upload_file(local_file, bucket, s3_file)
-            print("Upload Successful")
-            return True
-        except FileNotFoundError:
-            print("The file was not found")
-            return False
-        except NoCredentialsError:
-            print("Credentials not available")
-            return False
-
-    uploaded = upload_to_aws(f'output{i}.mp3', 'sleeplesslv', f'output{i}.mp3') # Upload to the sleepless AWS S3 bucket
     i = i + 1
+
+merged.export("final_output.wav", format="wav")
+uploaded = upload_to_aws('final_output.wav') # Upload the final file to the AWS S3 bucket
