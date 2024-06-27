3ab2083d15b73143b565ffc925883728dff2a4bb
Logan Valeski <loganvaleski@gmail.com>
2024-06-27 17:59:04 -0600

    Refactored days of week generation bash script

--- generateweek.sh
+++ generateweek.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-echo "hello"
+days=("Monday" "Tuesday" "Wednesday" "Thursday" "Friday" "Saturday" "Sunday")
+
+for day in "${days[@]}"; do
+  python3 gendriver.py 20 boring onyx male openai $day
+  python3 gendriver.py 20 boring nova female openai $day
+  python3 gendriver.py 20 meandering onyx male openai $day
+  python3 gendriver.py 20 meandering nova female openai $day
+done
