99c0095a7cdab0975e46af2b1c21bd26f2eb2683
Logan Valeski <loganvaleski@gmail.com>
2024-06-30 20:08:03 -0600

    Implemented generateweek tweak

--- generateweek.sh
+++ generateweek.sh
@@ -3,8 +3,6 @@
 days=("Saturday" "Sunday")
 
 for day in "${days[@]}"; do
-  python3 gendriver.py 20 boring onyx male openai $day
-  python3 gendriver.py 20 boring nova female openai $day
   python3 gendriver.py 20 meandering onyx male openai $day
   python3 gendriver.py 20 meandering nova female openai $day
 done
